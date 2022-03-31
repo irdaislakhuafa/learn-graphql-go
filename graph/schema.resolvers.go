e4d51d3ac9ab0b24a9a08783c08beafdb0b25b2e
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-04-01 02:19:49 +0700

    implement CreateTodo and get list of Todos in schema.resolver.go

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -6,17 +6,46 @@ package graph
 import (
 	"context"
 	"fmt"
+	"math/rand"
+	"strconv"
 
 	"github.com/irdaislakhuafa/learn-graphql-go/graph/generated"
 	"github.com/irdaislakhuafa/learn-graphql-go/graph/model"
 )
 
-func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+func (r *mutationResolver) CreateTodo(ctx context.Context, in model.NewTodo) (*model.Todo, error) {
+	defer func() {
+		err := recover()
+		if err != nil {
+			panic(fmt.Errorf("not implemented"))
+		}
+	}()
+
+	// mapping model.NewTodo to model.Todo
+	todo := &model.Todo{
+		ID:   strconv.Itoa(rand.Int()),
+		Text: in.Text,
+		User: &model.User{
+			ID:   in.UserID,
+			Name: "User " + in.UserID,
+		},
+	}
+
+	// add new model.Todo to slice `todos` (InMemoryDatabase)
+	r.todos = append(r.todos, todo)
+	return todo, nil
 }
 
 func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+	defer func() {
+		err := recover()
+		if err != nil {
+			panic(fmt.Errorf("not implemented"))
+		}
+	}()
+
+	// return list of todos from InMemoryDatabase
+	return r.todos, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
